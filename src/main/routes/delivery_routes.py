fb5d1c3763471926f5bda88f57c62069b400660c
TaynaCandidaSilva~ <tayna2507@gmail.com>
2025-01-14 11:12:46 -0300

    Implementando http_types no projeto

--- src/main/routes/delivery_routes.py
+++ src/main/routes/delivery_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+from src.main.http_types.http_request import HttpRequest
 
 delivery_routes_bp = Blueprint("delivery_routes_bp", __name__)
 
@@ -6,4 +7,5 @@ delivery_routes_bp = Blueprint("delivery_routes_bp", __name__)
 @delivery_routes_bp.route("/delivery/order", methods=["POST"])
 def registry_order():
     print(request.json)
+    http_request = HttpRequest(request.json)
     return jsonify({"ola": "mundo"}), 200
