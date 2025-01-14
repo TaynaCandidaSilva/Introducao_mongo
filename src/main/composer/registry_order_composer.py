2774124d823e54507dc55ceb3741d48aa9d741df
TaynaCandidaSilva~ <tayna2507@gmail.com>
2025-01-14 19:18:27 -0300

    Implementando registry_order_composer

---
+++ src/main/composer/registry_order_composer.py
@@ -0,0 +1,11 @@
+from src.user_cases.registry_order import RegisterOrder
+from src.models.repository.orders_repository import OrdersRepository
+from src.models.connection.connection_handler import db_connection_handler
+
+
+def registry_order_composer():
+    conn = db_connection_handler.get_db_connection()
+    model = OrdersRepository(conn)
+    use_case = RegisterOrder(model)
+
+    return use_case
