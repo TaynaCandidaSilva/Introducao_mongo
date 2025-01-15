3e3c8c42a77c2d88183d578dc67c2de200e429fe
TaynaCandidaSilva~ <tayna2507@gmail.com>
2025-01-15 07:49:40 -0300

    Implementando adicao de rota de caso de uso no registro de pedidos

--- src/main/routes/delivery_routes.py
+++ src/main/routes/delivery_routes.py
@@ -1,11 +1,15 @@
 from flask import Blueprint, jsonify, request
 from src.main.http_types.http_request import HttpRequest
 
+from src.main.composer.registry_order_composer import registry_order_composer
+
 delivery_routes_bp = Blueprint("delivery_routes_bp", __name__)
 
 
 @delivery_routes_bp.route("/delivery/order", methods=["POST"])
 def registry_order():
-    print(request.json)
-    http_request = HttpRequest(request.json)
-    return jsonify({"ola": "mundo"}), 200
+    user_case = registry_order_composer()
+    http_request = HttpRequest(body=request.json)
+    response = user_case.registry(http_request)
+
+    return jsonify(response.body), response.status_code
